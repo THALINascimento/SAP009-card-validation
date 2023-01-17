6e8e131b05b046e29382451a6fcced44586747c0
ThaliNascimento <thalia.secre@gmail.com>
2023-01-17 18:56:35 -0300

    iniciando css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+Document.queryselector('numero do cartoa').oninput = () =>{
+    document.queryselector('numero do cartao').innerText = document.querySelector('numero do cartoa').value;
+}
