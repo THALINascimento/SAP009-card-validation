770d1ece8810c9aba3d7072ea4cd7bdb80c51c24
ThaliNascimento <thalia.secre@gmail.com>
2023-01-28 22:11:15 -0300

    teste2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { isValid } from "./validator.js";
 
 function validar() {
   const card = document.getElementById("numero").value;
-  const validCard = isValid(card); //linka o resutado do input com a função
+  //const validCard = isValid(card); //linka o resutado do input com a função
   const result = document.getElementById("botao");
-  const maskfy = card;
+  //const maskfy = document.getElementById("numero");
+  const validade = document.querySelector(".cod");
+  const botao = document.getElementById("botao");
+  addEventListener("click", isValid);
 
   if (card === true) {
     result.innerHTML = "Cartão válido!";
@@ -13,4 +16,17 @@ function validar() {
   }
 }
 
-const botao = addEventListener("click");
+//const newNumber = parseInt(?[i]) + parseInt(?[1]);
+
+//maskify (validar) {
+//const maskNum = [];
+//for (let i = 0; i < validar.length; i++) {
+//if (i < validar.length - 4) {
+//maskNum.push("#");
+////}
+//else{
+//maskNum.push(validar[i]);
+//}
+///}
+//return maskNum.join("");
+//}
--- src/validator.js
+++ src/validator.js
@@ -1,22 +1,19 @@
-export function isValid (validar) {
-  const arrNcartao = digit.split("").reverse();//let invert = digit.split("").reverse();
-  console.log(arrNcartao); //numeros digitados invertidos tem outro jeito?; criar var arr () = invert const cardArray = []; //como colocar no arr para manipular posições ?
-  // = >= 48 && <= 57 //a identificação no Js dos  caracteres são numeros para aceitar somente numeros[0-9] usar padrão patern ?
+export function isValid(validar) {
+  const invert = digit.split("").reverse();
+  console.log(invert); //numeros digitados invertidos tem outro jeito?; criar var arr () = invert const cardArray = []; //como colocar no arr para manipular posições ?
+
   let soma = 0;
-  for (let i = 0; i < arrNcartao.length; i++) {
-    //ao inves de i++ por  i +1//seleciona posição do array para percorrer e atribuir a condição desejada.
-    console.log("indice", i, arrNcartao[i]);
-    if(i !==0 && i %2 ===0);
-    //((i + 1) % 2 !== 0) { //simplificar se posicao par multiplicar se posicao impar some
-      if{
-      arrNcartao[i]= arrNcartao[i] *=2;
+  for (let i = 0; i < invert.length; i ++) {
+    //let digito = parseInt(validar[i]);
+    if ((i + 1) % 2 !== 0); { //simplificar se posicao par multiplicar se posicao impar some (i !== 0 && i % 2 === 0)
+      invert[i] = invert[i] *= 2;
       return digit;
-    }
-    if(arrNcartao[i] > 9) {
-      arrNcartao[i] = arrNcartao[i] - 9;
-      return digit % 10;
-    }
+    } else { 
+      //invert[i] > 9 
+      //invert[i] = invert[i] - 9;
+    
+    soma + soma= invert;
   }
+  return soma % 10 === 0;
 }
 
-//var newNumber = parseInt(?[0]) + parseInt(?[1]);
