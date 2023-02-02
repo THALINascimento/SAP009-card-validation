40f471140fbdf332f79affec2b60f63ae45a4727
ThaliNascimento <thalia.secre@gmail.com>
2023-02-02 18:28:11 -0300

    congratulations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import validator from "./validator.js";
-
 const botao = document.getElementById("botao");
 botao.addEventListener("click", cardValidator);
 const msg = document.getElementById("mensagem"); //quando clicar no botao vai retornar mensagem de acordo com o que foi inserido
@@ -7,16 +6,17 @@ const msg = document.getElementById("mensagem"); //quando clicar no botao vai re
 function cardValidator() {
   const recebeNum = document.getElementById("numero").value; // o que for digitado
   const result = validator.isValid(recebeNum); // o numero digitado passa pelo luhn
-  //const mask = validator.maskify(recebeNum); //mascara o numero digato
+  const maskNum = validator.maskify(recebeNum); //mascara o numero digato
+  //console.log(maskNum);
 
   if (recebeNum === "") {
     return (msg.innerHTML = "Digite os dados do cartão");
   } else if (recebeNum.length < 10) {
     return (msg.innerHTML = "Verifique o cartão e tente novamente");
   } else if (result === true) {
-    return (msg.innerHTML = "Cartão + maskify válido! ");
-  } else (result === false) {
-    return (msg.innerHTML = "Cartão inválido! Revise os dados e tente novamente.");
+    return (msg.innerHTML = "Cartão + maskify válido!" + maskNum);
+  } else {
+    return (msg.innerHTML =
+      "Cartão inválido! Revise os dados e tente novamente.");
   }
 }
-console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,40 +1,44 @@
 const validator = {
-  isValid: function isValid(recebeNum) {
+  //objeto
+  isValid: function (recebeNum) {
     //armazenar numeros digitados
     const numeroCartao = recebeNum.split("").map(Number);
-    const invert = numeroCartao.reverse();
-    //console.log(invert);
-
+    const invert = numeroCartao.reverse(); //console.log(invert);
     let result = 0;
     for (let i = 0; i < invert.length; i++) {
-      //console.log("", i, invert[i]);
+      //console.log("teste for");
 
       if ((i + 1) % 2 === 0) {
         //(i % 2 === 0) { //posso fazer  i &&
         const multip = invert[i] * 2;
-        console.log(multip);
 
-        if (multip > 10) {
+        if (multip < 10) {
           result += multip; //;
-          console.log(subtrai);
-        } else if (multip <= 10) {
-          result += multip - 9;
-        } else {
-          result += parseInt(numeroCartao[i]);
-        }
-        console.log("FINal");
-      }
-
-      if (result % 10 === 0) {
-        return true;
+          //console.log("subtrai");
+        } else if (multip >= 10) result += multip - 9;
+        //console.log("meuDeus");
       } else {
-        return false;
+        result += parseInt(numeroCartao[i]);
       }
     }
+    //luhn lindo
+    if (result % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
   },
-  //maskify: function maskify(recebeNum){
-
-  //}
+  maskify: function (recebeNum) {
+    const mascara = [];
+    for (let i = 0; i < recebeNum.length; i++) {
+      if (i < recebeNum.length - 4) {
+        mascara.push("#");
+      } else {
+        mascara.push(recebeNum[i]);
+      }
+    }
+    const maskNum = mascara.join("");
+    return maskNum;
+  },
 };
-
 export default validator;
